740f4d52f1915767a1d13e36ca5e1441f6f0a161
Yohar Ruiz <yohar.ruiz@cdiproject.com>
2020-01-04 21:39:03 -0500

    finished exercise

--- app/src/main/java/com/example/appexercise2/MainActivity.java
+++ app/src/main/java/com/example/appexercise2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.appexercise2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -19,5 +20,18 @@ public class MainActivity extends AppCompatActivity {
         et2=(EditText)findViewById(R.id.txt_fisica);
         et3=(EditText)findViewById(R.id.txt_quimica);
         tv1=(TextView)findViewById(R.id.tv_estatus);
+    }
+    public void estatus(View view){
+        int matematica=Integer.parseInt(et1.getText().toString());
+        int fisica=Integer.parseInt(et2.getText().toString());
+        int quimica=Integer.parseInt(et3.getText().toString());
+
+        int promedio= (matematica+fisica+quimica)/3;
+
+        if(promedio>=6){
+            tv1.setText("Estatus Aprobado con "+promedio);
+        }else{
+            tv1.setText("Estatus Reprobado con "+promedio);
+        }
     }
 }
